476cd4bb2abddd4b659a662c3e3da83c30fa8976
raiasifali <asif.developer92@gmail.com>
2023-07-28 23:45:30 +0500

    Fixed class

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,19 @@
-import Layout from '@/components/layout'
-import Header from '@/components/header/header'
+import Layout from '@/components/layout';
+import Header from '@/components/header/header';
 export default function IndexPage() {
   return (
     <Layout title="Seliton">
       <main>
-        <div class="grid grid-cols-12">
-          <div class="border border-sky-500 bg-gray-100 col-span-2">
-            <p class="mt-1.5 text-sm text-gray-500">
-              Left sidebar 🚀
-            </p>
+        <div className="grid grid-cols-12">
+          <div className="border border-sky-500 bg-gray-100 col-span-2">
+            <p className="mt-1.5 text-sm text-gray-500">Left sidebar 🚀</p>
           </div>
 
-          <div class="col-span-10 bg-gray-100">
+          <div className="col-span-10 bg-gray-100">
             <Header />
           </div>
         </div>
       </main>
     </Layout>
-  )
+  );
 }
